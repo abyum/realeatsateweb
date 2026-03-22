a084e93c76163e1e96f769b45ea0b39023b864d4
abyum <abdulrashidhsn@gmail.com>
2026-03-23 01:49:14 +0300

    fix: remove static export causing 404

--- next.config.js
+++ next.config.js
@@ -24,20 +24,5 @@ const nextConfig = {
     buildActivity: false,
   },
 };
-if (process.env.NEXT_PUBLIC_SEO === "false") {
-  nextConfig.output = "export";
-  nextConfig.exportPathMap = async (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId },
-  ) => {
-    if (dir && outDir && fs.existsSync(path.join(dir, ".htaccess"))) {
-      fs.copyFileSync(
-        path.join(dir, ".htaccess"),
-        path.join(outDir, ".htaccess"),
-      );
-    } else {
-    }
-    return defaultPathMap;
-  };
-}
+
 module.exports = nextConfig;
